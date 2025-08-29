e9277577c89635087c89635b36d477708eac449b
Ubuntu <ubuntu@ip-172-31-40-57.ap-southeast-1.compute.internal>
2025-08-29 05:48:50 +0000

    update

---
+++ jenkins/deploy.groovy
@@ -0,0 +1,35 @@
+def getDockerTags() {
+    def tags = sh(
+        script: "curl -s https://hub.docker.com/repository/docker/orvencasido/resume-project/tags | jq -r '.results[].name'",
+        returnStdout: true
+    ).trim().split('\n')
+    return tags
+}
+
+pipeline {
+    agent { label 'docker-agent' }
+
+    parameters {
+        choice(
+            name: 'VERSION',
+            choices: getDockerTags(),
+            description: 'Select the version of the image'
+        )
+    }
+
+    environment {
+        DOCKER_IMAGE = "orvencasido/resume-project"
+    }
+
+    stages {
+        stage('Build') {
+            steps {
+                script {
+                    sh "docker build -t ${DOCKER_IMAGE}:${params.VERSION} ."
+                    sh "docker push ${DOCKER_IMAGE}:${params.VERSION}"
+                }
+            }
+        }
+    }
+}
+
