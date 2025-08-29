f8e8bf1fa460199588cd6e10c7c2f07c34776e70
Ubuntu <ubuntu@ip-172-31-40-57.ap-southeast-1.compute.internal>
2025-08-29 05:46:06 +0000

    update

--- jenkins/build.groovy
+++ jenkins/build.groovy
@@ -3,7 +3,7 @@ pipeline {
 
     environment {
         DOCKER_IMAGE = "orvencasido/resume-project"
-        VERSION = "${env.BUILD_NUMBER}"
+        VERSION = "1.${env.BUILD_NUMBER}"
     }
     
     stages {
@@ -11,6 +11,17 @@ pipeline {
             steps {
                 script {
                     sh "docker build -t ${DOCKER_IMAGE}:${VERSION} ."
+                }
+            }
+        }
+
+        stage('Push') {
+            steps {
+                withCredentials([usernamePassword(credentialsId: 'dockerhub-cred', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PW')]) {
+                    script {
+                        sh "echo ${DOCKER_PW} | docker login -u ${DOCKER_USER} --password-stdin"
+                        sh "docker push ${DOCKER_IMAGE}:${VERSION}"
+                    }
                 }
             }
         }
